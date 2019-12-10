ba0afbd3f1a8946d025060a157908e4639f77daa
BOTEA MOCANASU Daniel <daniel.botea-mocanasu@external.thalesgroup.com>
2019-12-10 16:11:14 +0200

    add content

---
+++ first-project/using-ts.ts
@@ -0,0 +1,17 @@
+const button = document.querySelector("button");
+const input1 = document.getElementById("num1")! as HTMLInputElement;
+const input2 = document.getElementById("num2")! as HTMLInputElement;
+
+function add(num1:number, num2:number) {
+  return num1 + num2;
+}
+
+button.addEventListener("click", function() { 
+  console.log(add(+input1.value, +input2.value));
+
+  const result: number = add(+input1.value, +input2.value)!;
+
+  const resultPrint:HTMLElement = document.getElementById('result');
+
+  resultPrint.innerText = result;
+});
